430ac14996b27c0a4e5c289699e0dafa9588365c
Nils Harbke <nils.harbke@proton.me>
2024-03-11 15:45:27 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "CVMake development";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = pkgs.mkShell {
+            TYPST_FONT_PATHS = "./fonts";
+            buildInputs = with pkgs; [
+              git-crypt
+              typst
+              typst-lsp
+              zathura
+              nixfmt
+            ];
+          };
+        }
+      );
+}
