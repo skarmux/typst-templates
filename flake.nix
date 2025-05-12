de1b43199fb0da5ac243a3e17de6fe608cebfa0b
Nils Harbke <nils.harbke@proton.me>
2025-05-12 13:03:04 +0200

    fighting the bash code checker

--- flake.nix
+++ flake.nix
@@ -12,15 +12,9 @@
           gum
         ];
         text = ''
-          #!/usr/bin/env bash
-          set -euo pipefail
-
-          # nix run [FLAKE] -- [FILE]
-          # FILE -- <name>.<lang>.<template>.typ
-
           if [ -z "$1" ]; then
             echo "<filename>.<lang>.<template>.toml"
-            read -p "Enter filename: " FILENAME
+            read -rp "Enter filename: " FILENAME
             echo "$FILENAME.<lang>.<template>.toml"
             LANG=$(gum choose "en" "de")
             echo "$FILENAME.$LANG.<template>.toml"
@@ -32,7 +26,7 @@
             echo "$FILENAME.$LANG.$TEMPLATE.toml"
             FILE="$FILENAME.$LANG.$TEMPLATE.toml"
             # Copy blank toml into current directory
-            cp "${self}/templates/data/blank/$TEMPLATE.toml" $FILE
+            cp "${self}/templates/data/blank/$TEMPLATE.toml" "$FILE"
           else
             FILE=$1
             if [[ "$FILE" =~ ^([^.]+)\.([^.]+)\.([^.]+)\.toml$ ]]; then
@@ -48,15 +42,15 @@
           # create temporary directory that serves as root for the typst compiler
           echo "preparing working dir for compilation"
           TYPST_ROOT=$(mktemp -d)
-          trap "rm -rf $TYPST_ROOT" EXIT
-          cp ${self}/templates/$TEMPLATE.typ "$TYPST_ROOT/$TEMPLATE.typ"
+          trap 'rm -rf $TYPST_ROOT' EXIT
+          cp "${self}/templates/$TEMPLATE.typ" "$TYPST_ROOT/$TEMPLATE.typ"
           cp -rv --no-preserve=all ${self}/templates/modules "$TYPST_ROOT/modules"
           # copy/symlink image assets to root
           echo "done"
 
           grep '^image *= *"' "$FILE" | sed -E 's/^image *= *"(.*)"/\1/' | while IFS= read -r filepath; do
               if [[ -f "$filepath" ]]; then
-                  base=$(dirname $FILE);
+                  base=$(dirname "$FILE");
                   mkdir -p "$TYPST_ROOT/$(dirname "$filepath")"
                   cp "$base/$filepath" "$TYPST_ROOT/$filepath"
               else
@@ -65,24 +59,24 @@
           done
 
           echo "setting up assets path"
-          ASSETS=''${ASSETS:-assets}
-          if [ ! -d $ASSETS ]; then
+          ASSETS="''${ASSETS:-assets}"
+          if [ ! -d "$ASSETS" ]; then
             echo "could not find assets directory at current location."
-            echo "checked at `pwd`/assets"
+            echo "checked at $(pwd)/assets"
             echo "please set the correct path in ASSETS=<path>."
             echo "falling back to sample assets instead"
             ASSETS=${self}/templates/assets
           fi
-          ln -s $ASSETS "$TYPST_ROOT/assets"
+          ln -s "$ASSETS" "$TYPST_ROOT/assets"
           echo "done"
 
           # move the file over into the working directory
           mkdir -v "$TYPST_ROOT/data"
-          cp -v $FILE "$TYPST_ROOT/data/$TEMPLATE.toml"
+          cp -v "$FILE" "$TYPST_ROOT/data/$TEMPLATE.toml"
         
           PDF="$FILENAME.$LANG.$TEMPLATE.pdf"
 
-          > "typst.log"
+          echo "" > "typst.log"
 
           # NOTE: `Symbols-Only` glyphs do not align well horizontally
           #       with all the other Font glyphs. Using ProFont instead.
@@ -90,22 +84,22 @@
           TYPST_FONT_PATHS="$TYPST_FONT_PATHS:${pkgs.noto-fonts-color-emoji}"
 
           ${pkgs.typst}/bin/typst watch \
-            --input lang=$LANG \
-            --input flavor=''${FLAVOR:-frappe} \
-            --input accent=''${ACCENT:-blue} \
-            --input label=''${LABEL:-false} \
-            --input assets=''${ASSETS:-assets} \
+            --input lang="$LANG" \
+            --input flavor="''${FLAVOR:-frappe}" \
+            --input accent="''${ACCENT:-blue}" \
+            --input label="''${LABEL:-false}" \
+            --input assets="''${ASSETS:-assets}" \
             --font-path $TYPST_FONT_PATHS \
             --ignore-system-fonts \
             "$TYPST_ROOT/$TEMPLATE.typ" \
-            $PDF \
+            "$PDF" \
             >> "typst.log" 2>&1 &
           WATCH_PID=$!
 
           $EDITOR "$TYPST_ROOT/data/$TEMPLATE.toml"
           # FIXME When the session crashes, changes won't make it back
           # to the original file and will be lost. Really lost!
-          cp -f "$TYPST_ROOT/data/$TEMPLATE.toml" $FILE
+          cp -f "$TYPST_ROOT/data/$TEMPLATE.toml" "$FILE"
 
           # let typst watch churn a little longer for the final changes
           # made to the toml. `:wq` is too quick of an exit <3
